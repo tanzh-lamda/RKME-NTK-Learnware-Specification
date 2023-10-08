cf58db784b680cd79ada7db7631f594edba1a29e
shihy <xingyuncao2002@outlook.com>
2023-10-08 13:25:17 +0800

    更新启动脚本

--- auto.bash
+++ auto.bash
@@ -1,9 +1,16 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1090
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate learnware_remote
 
-comm='--mode auto --n_uploaders 50 --n_users 50 -K 50'
+param='--mode auto --n_uploaders 50 --n_users 50 -K 50'
+folder="$(date +%s)"
+mkdir -p "./log/${folder}"
 
 for num in {0..6}
 do
-nohup python main.py --id ${num} ${comm} > "./log/auto_${num}.log" 2>&1 &
+# shellcheck disable=SC2086
+nohup python main.py --id ${num} ${param} > "./log/${folder}/auto_${num}.log" 2>&1 &
+echo $! > ./log/save_pid.txt
 done
